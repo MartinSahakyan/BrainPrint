b93cf1dcd1f531c0be91c796532c785e33d6463a
MartinSahakyan <martinsahakyan3@gmail.com>
2019-05-30 04:35:20 -0700

    some changes in Costum Controls

---
+++ BrainPrint/CustomControls/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  BrainPrint
+//
+//  Created by DarthMaul on 5/30/19.
+//  Copyright © 2019 DarthMaul. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

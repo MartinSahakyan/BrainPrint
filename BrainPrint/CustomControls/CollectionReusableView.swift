fb901e7e3b20ecc62bf47259f00ed9f673d04ad1
MartinSahakyan <martinsahakyan3@gmail.com>
2019-06-02 08:50:50 -0700

    add some changes

---
+++ BrainPrint/CustomControls/CollectionReusableView.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionReusableView.swift
+//  BrainPrint
+//
+//  Created by DarthMaul on 6/2/19.
+//  Copyright © 2019 DarthMaul. All rights reserved.
+//
+
+import UIKit
+
+class CollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
